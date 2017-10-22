388248ae239255750d5e639ce2a6945fb3594a14
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-10-22 19:04:38 +0200

    Redundant unit tests removed

--- main/tests.py
+++ main/tests.py
@@ -6,18 +6,6 @@ from main import views
 
 class HomeTest(TestCase):
 
-    def test_root_url_resolver_to_home_view(self):
-        resolved_view = resolve('/')
-        self.assertEqual(views.home, resolved_view.func)
-
-    def test_home_view_returns_correct_html(self):
-        request = HttpRequest()
-        response = views.home(request)
-        html = response.content.decode('utf-8')
-        self.assertTrue(html.startswith('<html>'))
-        self.assertIn('<title>Home</title>', html)
-        self.assertTrue(html.rstrip().endswith('</html>'))
-
     def test_uses_home_template(self):
         response = self.client.get('/')
         self.assertTemplateUsed(response, 'home.html')
