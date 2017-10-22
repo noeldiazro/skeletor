a04bc48b7b6569778061978a686d589219f6fd4b
Noel Díaz Rodríguez <noeldiazro@gmail.com>
2017-10-22 11:08:13 +0200

    Basic home view added

--- main/tests.py
+++ main/tests.py
@@ -1,3 +1,4 @@
+from django.http import HttpRequest
 from django.test import TestCase
 from django.urls import resolve
 from main import views
@@ -8,3 +9,11 @@ class HomeTest(TestCase):
     def test_root_url_resolver_to_home_view(self):
         resolved_view = resolve('/')
         self.assertEqual(views.home, resolved_view.func)
+
+    def test_home_view_returns_correct_html(self):
+        request = HttpRequest()
+        response = views.home(request)
+        html = response.content.decode('utf-8')
+        self.assertTrue(html.startswith('<html>'))
+        self.assertIn('<title>Home</title>', html)
+        self.assertTrue(html.endswith('</html>'))
